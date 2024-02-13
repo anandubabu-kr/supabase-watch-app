1259449d8cadb56f11310d2a5d2f17f2b9648fe5
Anandu Babu <anandubabu.sd@gmail.com>
2024-02-13 10:32:38 +0530

    initial commit with supabase config and route protection

---
+++ src/middleware.js
@@ -0,0 +1,28 @@
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { NextResponse } from "next/server";
+
+export async function middleware(req) {
+  const res = NextResponse.next();
+  const supabase = createMiddlewareClient({ req, res });
+
+  console.log("middleware");
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user && req.nextUrl.pathname === "/") {
+    console.log("not logged");
+    return NextResponse.redirect(new URL("/watch-list", req.url));
+  }
+
+  if (!user && req.nextUrl.pathname !== "/") {
+    console.log("logged");
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  return res;
+}
+
+export const config = {
+  matcher: ["/", "/watch-list"],
+};
