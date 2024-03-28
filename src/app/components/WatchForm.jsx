8ee825cdb257ea7b34aa50f92462e373fc00c6b6
Anandu Babu <anandubabu.sd@gmail.com>
2024-03-28 21:09:31 +0530

    feat(add-watch): add post watch to supabase

--- src/app/components/WatchForm.jsx
+++ src/app/components/WatchForm.jsx
@@ -1,7 +1,57 @@
 import React from "react";
+import { addWatch } from "../server-actions/addWatch";
 
 const WatchForm = () => {
-  return <div>WatchForm</div>;
+  return (
+    <form
+      action={addWatch}
+      className="flex flex-col justify-center align-middle bg-slate-50 p-2 m-3"
+    >
+      <h2 className=" text-center font-bold"> Add watch</h2>
+      <div className="flex flex-col  items-center mt-2">
+        <label className=" text-sm" htmlFor="brand">
+          Brand
+        </label>
+        <input
+          className=" bg-slate-100 max-w-[500px] rounded-md border border-slate-300 focus:outline-slate-400 p-1"
+          id="brand"
+          name="brand"
+          required
+          type="text"
+        />
+      </div>
+      <div className="flex flex-col  items-center mt-2">
+        <label className=" text-sm" htmlFor="modal">
+          Modal
+        </label>
+        <input
+          className=" bg-slate-100 max-w-[500px] rounded-md border border-slate-300 focus:outline-slate-400 p-1"
+          id="modal"
+          name="modal"
+          required
+          type="text"
+        />
+      </div>
+      <div className="flex flex-col  items-center mt-2">
+        <label className=" text-sm" htmlFor="referenceNumber">
+          Reference Number
+        </label>
+        <input
+          className=" bg-slate-100 max-w-[500px] rounded-md border border-slate-300 focus:outline-slate-400 p-1"
+          type="text"
+          id="referenceNumber"
+          name="referenceNumber"
+          required
+        />
+      </div>
+      <button
+        className=" bg-slate-400 w-fit mx-auto mt-4 px-4 py-2 rounded-md"
+        type="submit"
+      >
+        Submit
+      </button>
+    </form>
+  );
 };
 
 export default WatchForm;
--- src/app/watch-list/page.jsx
+++ src/app/watch-list/page.jsx
@@ -27,37 +27,44 @@ const WatchListPage = async () => {
 
   return (
     <div>
-      <h1 className=" text-center text-2xl">Watch List</h1>
-
-      <div>
-        <WatchForm />
+      <div className=" bg-slate-200 flex justify-between px-4 p-2 ">
+        <h1 className="text-2xl">My Watch List</h1>
+        <form
+          action="/auth/signout"
+          method="post"
+          className=" flex flex-col justify-center items-center"
+        >
+          <button
+            className=" bg-red-600 text-xs py-2 px-4 text-white rounded-md "
+            type="submit"
+          >
+            Sign Out
+          </button>
+        </form>
       </div>
 
-      <div>
-        {watches.map((watch) => (
-          <>
-            <h1>{watch}</h1>
+      <WatchForm />
 
-            <form action="deleteWatch">
+      <h2 className=" text-center text-lg">My list</h2>
+      <div className="flex flex-wrap">
+        {watches.map((watch) => (
+          <div className=" bg-slate-200 w-fit min-w-[300px] p-2 rounded-md border border-slate-300 m-2">
+            <p>Brand : {watch?.brand} </p>
+            <p>Modal: {watch?.modal} </p>
+            <p>Reference : {watch?.reference_number} </p>
+
+            <form className="flex justify-end" action="deleteWatch">
               <input type="hidden" name="id" id={watch.id} />
-              <button type="submit">delete</button>
+              <button
+                className=" bg-red-600 text-white text-xs py-1 px-4 rounded-md mt-4"
+                type="submit"
+              >
+                delete
+              </button>
             </form>
-          </>
+          </div>
         ))}
       </div>
-
-      <form
-        action="/auth/signout"
-        method="post"
-        className=" flex flex-col justify-center items-center"
-      >
-        <button
-          className=" bg-red-600 p-2 text-white rounded-md "
-          type="submit"
-        >
-          Sign Out
-        </button>
-      </form>
     </div>
   );
 };
